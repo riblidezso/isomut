3a928d60a3e2c7b302cb074c3b052fa4ae258baf
Ribli Dezso <ribli@jimgray87.inf.elte.hu>
2015-12-12 14:23:45 +0100

    made parallel more customizable

--- isomut_parallel.py
+++ isomut_parallel.py
@@ -49,43 +49,47 @@ def define_parallel_blocks(ref_genome,min_block_no):
 def run_isomut_on_block(chrom,from_pos,to_pos,
                          input_dir,bam_files,output_dir,
                          ref_genome,
-                         samtools='samtools',
-                         samtools_flags=' -B ',
                          min_sample_freq=0.2,
-                         min_other_freq=0.8,
+                         min_other_ref_freq=0.8,
                          cov_limit=10,
                          base_quality_limit=30,
                          min_gap_dist_snv=10,
                          min_gap_dist_indel=20):
     #build the command
-    cmd=samtools + ' mpileup  ' +samtools_flags + ' '
+    cmd=' samtools  mpileup -B ' 
     cmd+=' -f ' +ref_genome 
     cmd+=' -r '+chrom+':'+str(from_pos)+'-'+str(to_pos)+' '
     for bam_file in bam_files:
         cmd+=input_dir+bam_file +' '
     cmd+=' | ./unique_mutation_app '
-    cmd+=' '.join(map(str,[min_sample_freq,min_other_freq,cov_limit,
+    cmd+=' '.join(map(str,[min_sample_freq,min_other_ref_freq,cov_limit,
                            base_quality_limit,min_gap_dist_snv,min_gap_dist_indel])) +' '
     cmd+=' > ' +output_dir+'/'+ (chrom)+'_'+str(from_pos)+'_'+str(to_pos)+'_mut.csv  '
     
     return subprocess.check_output(cmd,shell=True,stderr=subprocess.STDOUT)
 
 
-
-def run_isomut_in_parallel(input_dir,bam_filenames,output_dir,ref_fasta, min_block_no=100,n_conc_blocks=2):
+def run_isomut_in_parallel(params):
     #define blocks and create args
-    blocks=define_parallel_blocks(ref_genome=ref_fasta,min_block_no=min_block_no)
-    args=[ (block[0],block[1],block[2],input_dir,bam_filenames,output_dir,ref_fasta) for block in blocks]
-   
+    blocks=define_parallel_blocks(params['ref_fasta'],params['n_min_block'])
+    args=[]
+    for block in blocks: 
+        args.append([ block[0],block[1],block[2],
+                     params['input_dir'],params['bam_filenames'],
+                     params['output_dir'],params['ref_fasta'],
+                     params['min_sample_freq'],params['min_other_ref_freq'],
+                     params['cov_limit'], params['base_quality_limit'],
+                     params['min_gap_dist_snv'],params['min_gap_dist_indel']])
+        
     #create dir
-    if(glob.glob(output_dir)==[]):
-        subprocess.call(['mkdir',output_dir])
+    if(glob.glob(params['output_dir'])==[]):
+        subprocess.call(['mkdir',params['output_dir']])
     
     print 'blocks to run:',len(args)
     print 'running:',
     #start first n concurrent block
     procs=[]
-    for i in xrange(n_conc_blocks):
+    for i in xrange(params['n_conc_blocks']):
         procs.append(multiprocessing.Process(target=run_isomut_on_block, args=args[len(procs)]))
         procs[-1].start()
         print len(procs),
@@ -93,7 +97,7 @@ def run_isomut_in_parallel(input_dir,bam_filenames,output_dir,ref_fasta, min_blo
         
     # when one finished restart teh next
     while (len(procs) != len(args)):
-        for i in xrange(1,n_conc_blocks+1):
+        for i in xrange(1,params['n_conc_blocks']+1):
             #one finished start another one
             if(procs[-i].is_alive() == False and len(procs) != len(args)):
                 procs.append(multiprocessing.Process(target=run_isomut_on_block,args=args[len(procs)] ))
